036457d0791d5e6bfc56dfff2e5c4a522469bd3a
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-16 09:33:53 +0100

    Add project files.

---
+++ Classes_And_Methods_Assigment_2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Classes_And_Methods_Assigment_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double resultOne = UsableFunctions.Power(5, 3);
+            double resultTwo = UsableFunctions.Power(-3, 1);
+            double resultThree = UsableFunctions.Power(14);
+
+            Console.WriteLine("base:5, power:3 = " + resultOne + "\n base:-3, power:1 = " + resultTwo + "\n base:14, power: 2(default) =" + resultThree);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Classes_And_Methods_Assigment_2/UsableFunctions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Classes_And_Methods_Assigment_2
+{
+    public class UsableFunctions
+    {
+        //the method very symiliar to Math.Pow() method, but if user won,t provide the second argument, it will be assigned to default 2 (second power)   
+        public static double Power(double baseNr, double power = 2)
+        {
+            double result = Math.Pow(baseNr, power);
+            return result;
+        }
+    }
+}
